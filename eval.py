b40f802d3621fc8bedb7ac5e34c365a0655535d3
Paul Rubenstein <pruben@login2.is.localnet>
2018-03-15 09:01:08 +0100

    added ilya evaluation code
    Signed-off-by: Paul Rubenstein <pruben@login2.is.localnet>

---
+++ eval.py
@@ -0,0 +1,152 @@
+import numpy as np
+import os
+import sys
+import logging
+import argparse
+from eval_utils import model_details
+from eval_utils import compute_metrics
+from eval_utils import compute_inception_stats
+from eval_utils import compute_blurriness
+from datahandler import DataHandler
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", default='..',
+                    help='Root path for the experiment folders')
+parser.add_argument("--recompute", default='no',
+                    help='If yes then dont reuse any pre-existing computations')
+parser.add_argument("--work_dir", default='.')
+parser.add_argument("--exp_name", default='celeba_',
+                    help='Substring specifying the name of experiments')
+parser.add_argument("--exp_dir_prefix", default='results_')
+parser.add_argument("--num_samples", default=1000,
+                    help='Number of samples for evaluating FID',
+                    type=int)
+parser.add_argument("--batch_size", default=500,
+                    type=int)
+
+FLAGS = parser.parse_args()
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+
+def main():
+    """
+    # 1. Go and look for the checkpoint folders in the specified paths
+        a. Specify the root directory
+        b. Select all subdirectories containing specified substring in the name
+    # 2. Check if the latest stored model was already FID-scored
+    # 3. If not --- compute its FIDs and save them in the separate files
+    # 4. Optionnaly aggregate all the FIDs together in a nice plot
+    """
+    assert FLAGS.num_samples % FLAGS.batch_size == 0, 'Number of samples should be factor of batch_size'
+    path = FLAGS.path
+    match = FLAGS.exp_name
+    prefix = FLAGS.exp_dir_prefix
+    to_process = []
+    models_fid = {}
+    # Get names of all entries in root path
+    names = os.listdir(path)
+    for name in names:
+        if not (prefix in name and match in name):
+            continue
+        if not os.path.isdir(os.path.join(path, name)):
+            continue
+        chdir = os.path.join(path, name, 'checkpoints')
+        if not os.path.isdir(chdir):
+            continue
+        model_names = os.listdir(chdir)
+        model_names = [n for n in model_names if '.meta' in n and n[0] != '.']
+        if len(model_names) == 0:
+            continue
+        key_f = lambda s: os.path.getctime(os.path.join(chdir, s))
+        newest_file = max(model_names, key=key_f)
+        newest_file = newest_file.split('.')[0]
+        fid_val_file = os.path.join(chdir, newest_file + '.fid' + str(FLAGS.num_samples) + '.val')
+        if os.path.exists(fid_val_file):
+            # The model was already evaluated
+            if FLAGS.recompute == 'no':
+                with open(fid_val_file, 'r') as f:
+                    lines = f.readlines()
+                fid_gen = float(lines[0].split('FID=')[1].split(',')[0])
+                fid_reconstr = float(lines[0].split('FID=')[2].split(',')[0])
+                logging.error('Model %s already processed' % chdir)
+                logging.error('FID=%f, computed using %d samples' % (fid, FLAGS.num_samples))
+                models_fid[os.path.join(path, name)] = (fid_gen, fid_reconstr)
+                continue
+            else:
+                os.remove(fid_val_file)
+        fid_temp_file = os.path.join(chdir, newest_file + '.fid' + str(FLAGS.num_samples) + '.tmp')
+        if os.path.exists(fid_temp_file):
+            # The model is being evaluated right now
+            continue
+        to_process.append((os.path.join(path, name), chdir, newest_file))
+
+    if len(to_process) > 0:
+        logging.error('Going to process the following models:')
+        for tup in to_process:
+            logging.error(os.path.join(tup[1], tup[2]))
+    else:
+        logging.error('Everything already processed.')
+
+    # Checking for the FID stats files for the required datasets
+    datasets = {}
+    for tup in to_process:
+        opts = model_details(os.path.join(tup[0], 'params.txt'),
+                             FLAGS.work_dir)
+        dataset = opts['dataset']
+        if dataset in datasets:
+            continue
+        datasets[dataset] = True
+        data_path = opts['data_dir']
+        stats_file = os.path.join(
+            data_path,
+            dataset + '.fidstats' + str(FLAGS.num_samples))
+        if os.path.exists(stats_file + '.npz'):
+            if FLAGS.recompute == 'yes':
+                os.remove(stats_file + '.npz')
+            else:
+                continue
+        logging.error('Computing FID stats for the %s dataset' % dataset)
+        data = DataHandler(opts)
+        random_ids = np.random.choice(data.num_points,
+                               FLAGS.num_samples, replace=False)
+        data_sample = data.data[random_ids]
+        if opts['input_normalize_sym']:
+            data_sample = data_sample / 2. + 0.5
+        data_sharpness = compute_blurriness(
+            data_sample[:min(500, FLAGS.num_samples)])
+        np.save(os.path.join(data_path, dataset + '.sharp' + \
+                str(FLAGS.num_samples) + '.npy'), data_sharpness)
+        logging.error('Dataset sharpness = %f' % np.mean(data_sharpness))
+        mu, cov = compute_inception_stats(data_sample, FLAGS.batch_size)
+        np.savez(stats_file, mu=mu, cov=cov)
+
+    for tup in to_process:
+        model = os.path.join(tup[1], tup[2])
+        logging.error('Processing %s' % model)
+        with open(os.path.join(tup[1], tup[2] + '.fid' + str(FLAGS.num_samples) + '.tmp'), 'w') as f:
+            f.write('In progress...')
+        # try:
+        fid = compute_metrics(tup, FLAGS.num_samples, FLAGS.work_dir, FLAGS.batch_size, FLAGS.recompute)
+        # except:
+        #     logging.error("Unexpected error:", sys.exc_info()[0])
+        #     logging.error('Failed to process %s' % model)
+        #     os.remove(os.path.join(tup[1], tup[2] + '.fid' + str(FLAGS.num_samples) + '.tmp'))
+        #     continue
+        if fid == None:
+            logging.error('Failed to process %s' % model)
+            os.remove(os.path.join(tup[1], tup[2] + '.fid' + str(FLAGS.num_samples) + '.tmp'))
+            continue
+        fid_gen, fid_reconstr = fid
+        with open(os.path.join(tup[1], tup[2] + '.fid' + str(FLAGS.num_samples) + '.val'), 'w') as f:
+            f.write('samples FID=%f, reconstruction FID=%f, computed using %d samples' % \
+                    (fid_gen, fid_reconstr, FLAGS.num_samples))
+            logging.error('samples FID=%f, reconstruction FID=%f, computed using %d samples' % \
+                    (fid_gen, fid_reconstr, FLAGS.num_samples))
+        models_fid[tup[0]] = (fid_gen, fid_reconstr)
+        os.remove(os.path.join(tup[1], tup[2] + '.fid' + str(FLAGS.num_samples) + '.tmp'))
+
+    logging.error(models_fid)
+
+main()
+
---
+++ eval_utils.py
@@ -0,0 +1,312 @@
+# Parts computing square root of a pd matrix and computing FID scores taken from 
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/gan/python/eval/python/classifier_metrics_impl.py
+# Parts manipulating Inception network taken from 
+# https://github.com/bioinf-jku/TTUR
+
+import tensorflow as tf
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import linalg_ops
+import logging
+import os
+import sys
+import numpy as np
+from datahandler import DataHandler
+import scipy.linalg as la
+
+CELEBA_DIR ='/lustre/home/pruben/GitRepos/Forks/wae/celebA/datasets/celeba/img_align_celeba'
+CIFAR10_DIR = '../cifar10'
+DSPRITES_DIR = '../dsprites'
+GRASSLI_DIR = '../grassli'
+MNIST_DIR = '../mnist'
+INCEPTION_PATH = '/lustre/home/pruben/GitRepos/FID-scores/classify_image_graph_def.pb'
+
+def compute_metrics(tup, num_samples, work_dir, batch_size, recompute):
+    # 1. Finding out details of the trained model
+    exp_path, model_path, model_filename = tup
+    param_file = os.path.join(exp_path, 'params.txt')
+    if not os.path.exists(param_file):
+        logging.error(' -- Directory %s does not contain params.txt' % exp_path)
+        return None
+    opts = model_details(param_file, work_dir)
+    # logging.error(opts)
+    # 2. Loading pre-computed dataset FID stats
+    stats = np.load(os.path.join(
+        opts['data_dir'],
+        opts['dataset'] + '.fidstats' + str(num_samples) + '.npz'))
+    data_mu, data_cov = stats['mu'], stats['cov']
+    # 3. Generating model samples and auto-encoding training samples
+    samples_path = os.path.join(
+        model_path, model_filename + '.samples' + str(num_samples) + '.npy')
+    reconstr_path = os.path.join(
+        model_path, model_filename + '.reconstr' + str(num_samples) + '.npy')
+    if os.path.exists(samples_path) and os.path.exists(reconstr_path) and recompute == 'no':
+        logging.error(' -- Samples and training reconstruction already available')
+        gen = np.load(samples_path)
+        train_reconstr = np.load(reconstr_path)
+    else:
+        logging.error(' -- Generating samples and training reconstructions')
+        data = DataHandler(opts)
+        random_ids = np.random.choice(data.num_points,
+                                      num_samples, replace=False)
+        to_autoencode = data.data[random_ids]
+        gen, train_reconstr = run_model(
+            model_path, model_filename, num_samples, opts, to_autoencode, batch_size)
+        if type(gen) is not np.ndarray:
+            return None
+        if opts['input_normalize_sym']:
+            gen = gen / 2. + 0.5
+            train_reconstr = train_reconstr / 2. + 0.5
+        sample_sharp = compute_blurriness(gen[:min(500, num_samples)])
+        reconstr_sharp = compute_blurriness(
+            train_reconstr[:min(500, num_samples)])
+        logging.error(' -- Samples sharpness = %f' % np.mean(sample_sharp))
+        logging.error(' -- Reconstruction sharpness = %f' % np.mean(reconstr_sharp))
+        np.save(os.path.join(model_path, model_filename + '.samples' + \
+                str(num_samples) + '.npy'), gen)
+        np.save(os.path.join(model_path, model_filename + '.reconstr' + \
+                str(num_samples) + '.npy'), train_reconstr)
+        np.savez(os.path.join(model_path, model_filename + '.sharp' + \
+                 str(num_samples)),
+                 sharp_gen=sample_sharp,
+                 sharp_reconstr=reconstr_sharp)
+    # 4. Computing FID of generated samples
+    logging.error(' -- Computing FID of generated samples')
+    fid_gen = fid_using_samples((data_mu, data_cov), gen, batch_size)
+    logging.error(' -- Computing FID of reconstructed training images')
+    fid_reconstr = fid_using_samples((data_mu, data_cov),
+                                     train_reconstr, batch_size)
+    if type(fid_gen) != tuple:
+        return None
+    np.savez(os.path.join(model_path, model_filename + '.fidstats' + \
+            str(num_samples)),
+            mu_gen=fid_gen[1],
+            cov_gen=fid_gen[2],
+            mu_rec=fid_reconstr[1],
+            cov_rec=fid_reconstr[2])
+    return fid_gen[0], fid_reconstr[0]
+
+def model_details(param_filename, work_dir):
+    with open(param_filename, 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        if 'zdim :' in line:
+            zdim = int(line.split(' : ')[-1])
+        if 'dataset :' in line:
+            dataset = line.split(' : ')[-1][:-1]
+        if 'pz_scale :' in line:
+            pz_scale = float(line.split(' : ')[-1])
+        if 'pz :' in line:
+            pz = line.split(' : ')[-1][:-1]
+        if 'input_normalize_sym :' in line:
+            normalize = ('True' in line)
+    opts = {}
+    opts['dataset'] = dataset
+    opts['input_normalize_sym'] = normalize
+    opts['work_dir'] = work_dir
+    opts['celebA_crop'] = 'closecrop'
+    opts['pz'] = pz
+    opts['zdim'] = zdim
+    opts['pz_scale'] = pz_scale
+    if dataset == 'celebA':
+        opts['data_dir'] = CELEBA_DIR
+    elif dataset == 'cifar10':
+        opts['data_dir'] = CIFAR10_DIR
+    elif dataset == 'dsprites':
+        opts['data_dir'] = DSPRITES_DIR
+    elif dataset == 'grassli':
+        opts['data_dir'] = GRASSLI_DIR
+    elif dataset == 'mnist':
+        opts['data_dir'] = MNIST_DIR
+    return opts
+
+def run_model(path, filename, num_samples, opts, to_reconstr, batch_size):
+    with tf.Session() as sess:
+        with sess.graph.as_default():
+            # try:
+            saver = tf.train.import_meta_graph(
+                os.path.join(path, filename + '.meta'))
+            saver.restore(sess, os.path.join(path, filename))
+            real_points_ph = tf.get_collection('real_points_ph')[0]
+            noise_ph = tf.get_collection('noise_ph')[0]
+            is_training_ph = tf.get_collection('is_training_ph')[0]
+            decoder = tf.get_collection('decoder')[0]
+            encoder = tf.get_collection('encoder')
+            if len(encoder) > 0:
+                encoder = encoder[0]
+                encoder_log_sigma = None
+            else:
+                # VAE model
+                encoder = tf.get_collection('encoder_mean')[0]
+                encoder_log_sigma = tf.get_collection('encoder_log_sigma')[0]
+            # except:
+            #     logging.error(" -- Unexpected error:", sys.exc_info()[0])
+            #     return None
+            zdim = opts['zdim']
+            pz = opts['pz']
+            pz_scale = opts['pz_scale']
+
+            # 1. Generating random samples
+            if pz == 'uniform':
+                noise = np.random.uniform(-1, 1, [num_samples, zdim])
+            elif pz in ('normal', 'sphere'):
+                noise = np.random.randn(num_samples, zdim)
+                if pz == 'sphere':
+                    noise = noise / np.sqrt(
+                        np.sum(noise * noise, axis=1))[:, np.newaxis]
+            z = pz_scale * noise
+            z = np.reshape(z, [-1, zdim])
+            gen = []
+            for ibatch in xrange(num_samples / batch_size):
+                batch = z[ibatch * batch_size : (ibatch + 1) * batch_size]
+                try:
+                    gen_batch = sess.run(
+                        decoder,
+                        feed_dict={noise_ph: batch, is_training_ph: False})
+                except:
+                    logging.error(" -- Unexpected error:", sys.exc_info()[0])
+                    return None
+                gen.append(gen_batch)
+            gen = np.vstack(gen)
+
+            # 2. Auto-encoding training pictures
+            reconstr = []
+            for ibatch in xrange(num_samples / batch_size):
+                batch = to_reconstr[ibatch * batch_size : (ibatch + 1) * batch_size]
+                # try:
+                encoded_batch = sess.run(
+                    encoder, feed_dict={real_points_ph: batch,
+                                        is_training_ph: False})
+                if encoder_log_sigma != None:
+                    # We have VAE, need to add the scaled noise
+                    batch_log_sigma = sess.run(
+                        encoder_log_sigma, feed_dict={real_points_ph: batch,
+                                                      is_training_ph: False})
+                    noise = z[ibatch * batch_size : (ibatch + 1) * batch_size]
+                    scaled_noise = np.multiply(np.exp(batch_log_sigma / 2.), noise)
+                    encoded_batch += scaled_noise
+
+                reconstructed_batch = sess.run(
+                    decoder, feed_dict={noise_ph: encoded_batch,
+                                        is_training_ph: False})
+                # except:
+                #     logging.error(" -- Unexpected error:", sys.exc_info()[0])
+                #     return None
+                reconstr.append(reconstructed_batch)
+            reconstr = np.vstack(reconstr)
+
+    tf.reset_default_graph()
+    return gen, reconstr
+
+# code for handling inception net derived from
+#   https://github.com/openai/improved-gan/blob/master/inception_score/model.py
+def _get_inception_layer(sess):
+    """Prepares inception net for batched usage and returns pool_3 layer. """
+    layername = 'FID_Inception_Net/pool_3:0'
+    pool3 = sess.graph.get_tensor_by_name(layername)
+    ops = pool3.graph.get_operations()
+    for op_idx, op in enumerate(ops):
+        for o in op.outputs:
+            shape = o.get_shape()
+            if shape._dims is not None:
+              shape = [s.value for s in shape]
+              new_shape = []
+              for j, s in enumerate(shape):
+                if s == 1 and j == 0:
+                  new_shape.append(None)
+                else:
+                  new_shape.append(s)
+              o._shape = tf.TensorShape(new_shape)
+    return pool3
+
+def compute_inception_stats(points, batch_size, _sess=None):
+    if _sess:
+        sess = _sess
+    else:
+        sess = tf.Session()
+    # Safe to assume len(points) > batch_size and divisible bybatch_size 
+    path = INCEPTION_PATH
+    with tf.gfile.FastGFile(path, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='FID_Inception_Net')
+
+    sess.run(tf.global_variables_initializer())
+    inception_op = _get_inception_layer(sess)
+    res = []
+    for ibatch in range(len(points) / batch_size):
+        # logging.error(' -- Propagating batch %d / %d' % (ibatch + 1, len(points) / batch_size))
+        batch = 256 * points[ibatch * batch_size:(ibatch + 1) * batch_size]
+        batch_prop = sess.run(
+            inception_op, feed_dict={'FID_Inception_Net/ExpandDims:0': batch})
+        res.append(batch_prop.reshape(batch_size, -1))
+    res = np.vstack(res)
+
+    mu = np.mean(res, axis=0)
+    cov = np.cov(res, rowvar=False)
+
+    if not _sess:
+        sess.close()
+        tf.reset_default_graph()
+
+    return mu, cov
+
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/gan/python/eval/python/classifier_metrics_impl.py
+def sym_matrix_sqrt(mat):
+    sval, umat, vmat = linalg_ops.svd(mat)
+    si = array_ops.where(math_ops.less(sval, 1e-10),
+                         sval, math_ops.sqrt(sval))
+    return math_ops.matmul(math_ops.matmul(
+        umat, array_ops.diag(si)), vmat, transpose_b=True)
+
+# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/gan/python/eval/python/classifier_metrics_impl.py
+def trace_sqrt_product(cov1, cov2):
+    sqrt_cov1 = sym_matrix_sqrt(cov1)
+    temp = math_ops.matmul(
+        sqrt_cov1, math_ops.matmul(cov2, sqrt_cov1))
+
+    return math_ops.trace(sym_matrix_sqrt(temp))
+
+def fid_using_samples(real, gen, batch_size):
+    data_mu, data_cov = real
+
+    with tf.Session() as sess:
+        logging.error(' -- Computing Inception stats')
+        gen_mu, gen_cov = compute_inception_stats(gen, batch_size, sess)
+        logging.error(' -- Computing Frechet distance')
+        m = np.sum(np.square(data_mu - gen_mu))
+        s = trace_sqrt_product(data_cov, gen_cov)
+        s = sess.run(s)
+        trace_data = np.trace(data_cov)
+        trace_gen = np.trace(gen_cov)
+        dist = m + trace_data + trace_gen - 2.0 * s
+        # logging.error('Mean=%f, trace_data=%f, trace_gen=%f, trace_sqrt=%f' %\
+        #        (m, trace_data, trace_gen, s))
+
+    if np.isnan(dist):
+        logging.error(' -- NaNs appeared while computing FID')
+        return None
+
+    tf.reset_default_graph()
+    return (dist, gen_mu, gen_cov)
+
+def compute_blurriness(images):
+    with tf.Session() as sess:
+        with sess.graph.as_default():
+            sample_size = images.shape[0]
+            # First convert to greyscale
+            if images.shape[-1] > 1:
+                # We have RGB
+                images = tf.image.rgb_to_grayscale(images)
+            images = tf.cast(images, tf.float32)
+            # Next convolve with the Laplace filter
+            lap_filter = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+            lap_filter = lap_filter.reshape([3, 3, 1, 1]).astype(np.float32)
+            conv = tf.nn.conv2d(images, lap_filter,
+                                strides=[1, 1, 1, 1], padding='VALID')
+            # And finally get the variance
+            _, lapvar = tf.nn.moments(conv, axes=[1, 2, 3])
+            # run the op
+            blurr = sess.run(lapvar)
+    tf.reset_default_graph()
+    return blurr
